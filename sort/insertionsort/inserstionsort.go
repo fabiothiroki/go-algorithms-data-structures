ac7f093b18c21714908fbd3eff016379565e0193
Fabio Hiroki <fabio.hiroki@quintoandar.com.br>
2021-04-20 21:02:40 -0300

    fix solution

--- sort/insertionsort/inserstionsort.go
+++ sort/insertionsort/inserstionsort.go
@@ -5,9 +5,8 @@ package insertionsort
 // Best: time O(n) space O(1)
 func InsertionSort(array []int) []int {
 
-	for i := 1; i < len(array)-1; i++ {
-
-		for j := i; j >= 0 && array[j-1] < array[j]; j-- {
+	for i := range array {
+		for j := i; j > 0 && array[j-1] > array[j]; j-- {
 			array[j-1], array[j] = array[j], array[j-1]
 		}
 	}
